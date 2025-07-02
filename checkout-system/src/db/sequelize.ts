8109a34946b43dd91f6255ccafe17200d9739b31
Sukanya-2000 <bhattacharyyasukanya5@gmail.com>
2025-07-02 21:26:53 +0530

    fixed database sync issue

--- checkout-system/src/db/sequelize.ts
+++ checkout-system/src/db/sequelize.ts
@@ -4,6 +4,13 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+console.log('DB ENV VALUES:', {
+    DB_NAME: process.env.DB_NAME,
+    DB_USER: process.env.DB_USER,
+    DB_PASS: process.env.DB_PASS,
+    DB_HOST: process.env.DB_HOST,
+  });
+
 if (
   !process.env.DB_NAME ||
   !process.env.DB_USER ||
@@ -23,5 +30,7 @@ const sequelize = new Sequelize(
     logging: false
   }
 );
+
+
 
 export default sequelize;
